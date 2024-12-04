2b270b518d84cb1f66b029cf87fdbcdb1b970d93
VojtechPeschel <vojtapeschel@gmail.com>
2024-12-04 19:48:28 +0100

    Initial commit from Create Next App

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+	useTabs: true,
+	singleQuote: true,
+	quoteProps: 'consistent',
+	trailingComma: 'none',
+	arrowParens: 'avoid',
+	endOfLine: 'auto',
+
+	plugins: ['prettier-plugin-tailwindcss']
+};
