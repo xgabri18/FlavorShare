6475146f05014c9fc2429027f090dd1f9fb638a6
Mário Novák <493094@mail.muni.cz>
2024-12-17 14:54:13 +0100

    Remove useless user check

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,9 +1,6 @@
 import { createUploadthing, type FileRouter } from 'uploadthing/next';
-import { UploadThingError } from 'uploadthing/server';
 
 const f = createUploadthing();
-
-// const auth = (req: Request) => ({ id: 'fakeId' }); // Fake auth function
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const imageFileRouter = {
@@ -22,17 +19,6 @@ export const imageFileRouter = {
 		{ awaitServerData: true }
 	)
 		// Set permissions and file types for this FileRoute
-		.middleware(async () => {
-			// async ({ req }) => {
-			// This code runs on your server before upload
-			const user = { id: 'fakeId' };
-
-			// If you throw, the user will not be able to upload
-			if (!user) throw new UploadThingError('Unauthorized');
-
-			// Whatever is returned here is accessible in onUploadComplete as `metadata`
-			return { userId: user.id };
-		})
 		.onUploadError(({ error, fileKey }) => {
 			console.log(
 				`While uploading ${fileKey} an error occurred: ${error.message}`
@@ -40,13 +26,11 @@ export const imageFileRouter = {
 		})
 		.onUploadComplete(async ({ metadata, file }) => {
 			// This code RUNS ON YOUR SERVER after upload
-			console.log('Upload complete for userId:', metadata.userId);
 
 			console.log('file url', file.url);
 
 			// !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
 			return {
-				uploadedBy: metadata.userId,
 				file_key: file.key,
 				file_url: file.url
 			};
